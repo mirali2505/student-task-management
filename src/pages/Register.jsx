3b42c963993dcc1402a55c02e1ba06906d9fbb67
mirali vadhiya <miralivadhiya25@gmail.com>
2026-01-30 13:51:08 +0530

    [mv][css add]

---
+++ src/pages/Register.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Register = () => {
+  return (
+    <>
+    <div class="form-container">
+         <h1>this is register</h1>
+    </div>
+    
+    </>
+  )
+}
+
+export default Register
