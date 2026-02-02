179ff50812d5f457867957e444923b53427cba24
mirali vadhiya <miralivadhiya25@gmail.com>
2026-02-02 13:32:13 +0530

    [mv][default router added]

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,33 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import './App.css'
-import Header from './components/Header'
-import Login from './pages/Login'
-import Register from './pages/Register'
+import { createBrowserRouter, Navigate, RouterProvider } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
+const DefaultRoute = () => {
+  const authData = JSON.parse(localStorage.getItem("authData"));
+  if (authData) {
+    return <Navigate to="/login" replace></Navigate>;
+  }
+  return <Navigate to="/register" replace></Navigate>;
+};
 function App() {
-  const route=createBrowserRouter([
+  const route = createBrowserRouter([
     {
-      path:"/login",
-      element:<Login />
+      path: "/",
+      element: <DefaultRoute />,
     },
     {
-      path:"/register",
-      element:<Register />
-    }
-  ])
-  
-  return (
-    < RouterProvider router={route} />
-     
-    
-  )
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/register",
+      element: <Register />,
+    },
+  ]);
+
+  return <RouterProvider router={route} />;
 }
 
-export default App
+export default App;
